c2003ece8613ae87d0793a8dc9b16f58d7fbe605
Eric Berglund <ericberglund117@gmail.com>
2020-08-18 17:43:22 -0600

    Create Round files

---
+++ test/Deck-test.js
@@ -0,0 +1,91 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const prototypeData = require('../src/Data');
+
+describe('Deck', function() {
+
+  it('should be a function', () => {
+    expect(Deck).to.be.a('function');
+  });
+
+  it('should be an instance of Deck', () => {
+    const deck = new Deck();
+    expect(deck).to.be.an.instanceof(Deck);
+  });
+
+  it('should be initialized with an array of card objects', () => {
+    const card1 = new Card(
+      1,
+      'What is Robbie\'s favorite animal',
+      ['sea otter', 'pug', 'capybara'],
+      'sea otter');
+    const card2 = new Card(
+      14,
+      'What organ is Khalid missing?',
+      ['spleen', 'appendix', 'gallbladder'],
+      'gallbladder');
+    const card3 = new Card(
+      12,
+      'What is Travis\'s middle name?',
+      ['Lex', 'William', 'Fitzgerald'],
+      'Fitzgerald');
+
+    const deck = new Deck([card1, card2, card3]);
+    expect(deck.cards).to.deep.equal([card1, card2, card3]);
+  });
+
+  it('should store information from card objects', () => {
+    const card1 = new Card(
+      1,
+      'What is Robbie\'s favorite animal',
+      ['sea otter', 'pug', 'capybara'],
+      'sea otter');
+    const card2 = new Card(
+      14,
+      'What organ is Khalid missing?',
+      ['spleen', 'appendix', 'gallbladder'],
+      'gallbladder');
+    const card3 = new Card(
+      12,
+      'What is Travis\'s middle name?',
+      ['Lex', 'William', 'Fitzgerald'],
+      'Fitzgerald');
+
+    const deck = new Deck([card1, card2, card3]);
+    expect(deck.cards[1].id).to.equal(14);
+  });
+
+  it('should tell how many cards are in the deck', () => {
+    const card1 = new Card(
+      1,
+      'What is Robbie\'s favorite animal',
+      ['sea otter', 'pug', 'capybara'],
+      'sea otter');
+    const card2 = new Card(
+      14,
+      'What organ is Khalid missing?',
+      ['spleen', 'appendix', 'gallbladder'],
+      'gallbladder');
+    const card3 = new Card(
+      12,
+      'What is Travis\'s middle name?',
+      ['Lex', 'William', 'Fitzgerald'],
+      'Fitzgerald');
+
+    const deck = new Deck([card1, card2, card3]);
+    const amountOfCards = deck.countCards();
+    expect(amountOfCards).to.equal(3);
+  });
+
+  it('should still tell how many cards are in the deck out of the possible amount of cards', () => {
+   const data = prototypeData.prototypeData;
+   const deck = new Deck(data);
+   const amountOfCards = deck.countCards();
+
+   expect(amountOfCards).to.equal(30);
+  });
+
+});
