2c33b4fcfca3376408765296800a008ec94ff861
Eric Berglund <ericberglund117@gmail.com>
2020-08-19 20:17:54 -0600

    Pass first three tests

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,14 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Round = require('./Round');
+const Card = require('./Card');
+const Deck = require('./Deck');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = null;
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
---
+++ test/Game-test.js
@@ -0,0 +1,23 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Game = require('../src/Game');
+const Round = require('../src/Round');
+
+describe('Game', function() {
+  it('should be a function', () => {
+    expect(Game).to.be.a('function');
+  });
+
+  it('should be an instance of Game', () => {
+    const game = new Game();
+    expect(game).to.be.an.instanceof(Game);
+  });
+
+  it('should keep track of the currentRound', () => {
+    const game = new Game();
+    expect(game.currentRound).to.equal(null);
+  });
+
+  
+});
